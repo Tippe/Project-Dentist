603887cd3cb0486990bc107d7dd99897b63e185b
Lars Luijendijk <larsluijendijk@gmail.com>
2017-09-05 09:40:46 +0200

    head fix

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,21 +1,19 @@
 <html>
 	<head>
-		<title>ciBlog</title>
+		<title>Dentist</title>
 		<link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css">
 	</head>
 	<body>
 	<nav class="navbar navbar-inverse">
       <div class="container">
         <div class="navbar-header">
-          <a class="navbar-brand" href="</">ciBlog</a>
+          <a class="navbar-brand" href="">Dentist</a>
         </div>
         <div id="navbar">
           <ul class="nav navbar-nav">
-            <li><a href="#">Home</a></li> <!-- ?php echo base_url(); ? -->
+            <li><a href="home">Home</a></li> <!-- ?php echo base_url(); ? -->
             <li><a href="login">Login</a></li>
           </ul>
         </div>
       </div>
     </nav>
-
-     
