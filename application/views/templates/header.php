5fadb0e526ddfa25df56ed4565d9b05acd2fc2d8
Tippe van Roosmalen <tippevanroosmalen@outlook.com>
2017-09-05 09:42:26 +0200

    bootstrap added in the templates/header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,7 +1,8 @@
 <html>
-	<head>
+  <head>
 		<title>ciBlog</title>
-		<link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 	</head>
 	<body>
 	<nav class="navbar navbar-inverse">
@@ -17,5 +18,3 @@
         </div>
       </div>
     </nav>
-
-     
