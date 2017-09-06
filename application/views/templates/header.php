c0d47827142ed750386f132b2d49f2b9e64dda69
Armania50 <arya.00767@gmail.com>
2017-09-06 14:31:56 +0200

    Codeigniter is created, with short url's. There is also an login/register system created.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,34 +1,21 @@
 <html>
-  <head>
-		<title>Dentist</title>
-    <link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	<head>
+		<title>ciBlog</title>
+		<link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css">
 	</head>
-	<body style="font-size:16pt;">
-<nav class="navbar navbar-toggleable-md navbar-inverse bg-primary">
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <a class="navbar-brand" href="#">Navbar</a>
+	<body>
+	<nav class="navbar navbar-inverse">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="</">ciBlog</a>
+        </div>
+        <div id="navbar">
+          <ul class="nav navbar-nav">
+            <li><a href="#">Home</a></li> <!-- ?php echo base_url(); ? -->
+            <li><a href="login">Login</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 
-    <div class="collapse navbar-collapse" id="navbarColor02">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="Home">Home </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="Login">Login</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">nothing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">nothing</a>
-        </li>
-      </ul>
-      <form class="form-inline">
-        <input class="form-control mr-sm-2" type="text" placeholder="Search">
-        <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
-      </form>
-    </div>
-  </nav>
+     
