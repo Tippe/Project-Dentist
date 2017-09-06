935fc01d1ac5adf0252ed86d1aa068b387517633
Lars Luijendijk <larsluijendijk@gmail.com>
2017-09-06 14:44:07 +0200

    delete merge error

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,6 +1,4 @@
 <h2><?= $title ?></h2>
-<<<<<<< HEAD
-<p>Welcome! This is the Home Page. Made by lars.</p>
-=======
-<p>Welcome to this professional dentist website m8</p>
->>>>>>> c0d47827142ed750386f132b2d49f2b9e64dda69
+
+<p>Welcome! This is the Home Page.</p>
+
