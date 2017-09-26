b3144dff52e0616f16d2c70455ce90135b68f0a4
Tippe van Roosmalen <tippevanroosmalen@outlook.com>
2017-09-26 09:42:18 +0200

    merge fix

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,7 +2,9 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $route['default_controller'] = 'pages/view';
-$route['(:any)'] = 'pages/view/$1'; // $1 represents anything
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
 $route['appointments/view/(:any)'] = 'appointments/view/$1'; // $1 represents anything
+$route['account/account_details/(:any)'] = 'account/account_details/$1'; // $1 represents anything
+$route['(:any)'] = 'pages/view/$1'; // $1 represents anything
