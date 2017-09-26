ee4d495ef05e9690ae9a228ed1fb12e88d194288
Lars Luijendijk <larsluijendijk@gmail.com>
2017-09-26 09:51:31 +0200

    merge conflict fixer

--- application/config/routes.php
+++ application/config/routes.php
@@ -4,12 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'pages/view';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-<<<<<<< HEAD
-
 $route['appointments/view/(:any)'] = 'appointments/view/$1'; // $1 represents anything
 $route['account/account_details/(:any)'] = 'account/account_details/$1'; // $1 represents anything
 $route['(:any)'] = 'pages/view/$1'; // $1 represents anything
-=======
-$route['appointments/view/(:any)'] = 'appointments/view/$1'; // $1 represents anything
 $route['backend'] = 'backend/index';
->>>>>>> 8b6d7cd9d0a1d963a085ca2bb9cda8c181d0ec0a
