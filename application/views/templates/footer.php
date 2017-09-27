d431af8b88feb00659cf509fde44d9a756511338
Tippe van Roosmalen <tippevanroosmalen@outlook.com>
2017-09-27 10:08:57 +0200

    better var_dump in footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -2,7 +2,9 @@
 	        <div class="footer">
 	          <a href=https://github.com/Tippe/Project-Dentist/graphs/contributors>Website created by the Code Addicts</a>
 	          <?php
-	          var_dump($_SESSION);
+	          echo "<pre>";
+	          var_dump($this->session);
+	          echo "</pre>";
 	          ?>
 	        </div>
 </footer>
