dea45be32ff50693d03cc87664bdda035b6811cb
Armania50 <arya.00767@gmail.com>
2017-10-24 12:15:32 +0200

    backend titel komt overeen met de gekozen pagina (zoals "users")
    backend titel komt overeen met de gekozen pagina (zoals "users")

--- application/views/templates/backend_header.php
+++ application/views/templates/backend_header.php
@@ -7,7 +7,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Starter</title>
+  <title>Dentist</title>
   <!-- Tell the browser to be responsive to screen width -->
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
