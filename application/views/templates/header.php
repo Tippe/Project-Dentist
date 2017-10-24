80f15aa00a5e32b5cea385af187da07c72cae923
Lars Luijendijk <larsluijendijk@gmail.com>
2017-10-24 12:15:07 +0200

    Changed the header, now its fancy :)

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,35 +2,60 @@
 <html>
   <head>
     <title>Dentist</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css">
     <link href="<?php echo base_url(); ?>assets/css/style.css" rel='stylesheet' type='text/css' />
     <link rel="icon" href="<?php echo base_url(); ?>assets/img/brackets.png" type="image/gif" sizes="16x16">
   </head>
   <body>
-  <nav class="navbar navbar-inverse">
-      <div class="container">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="<?php echo base_url()?>home">Dentist(*)</a>
-        </div>
-        <div id="navbar">
-          <ul class="nav navbar-nav">
-            <?php
+
+    <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand navbar-hover" href="<?php echo base_url()?>home">Dentist</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <?php
             if ($this->session->userdata('user_logged') == true) {?>
-              <li><a href="<?php echo base_url()?>home">Home</a></li>
-              <li><a href="<?php echo base_url()?>appointments/index">Appointments</a></li>
-              <li><a href="<?php echo base_url()?>auth/logout">Logout</a></li>
-            <?php
-            if ($this->session->userdata('role_id') == '99') {?>
-             <li><a href="<?php echo base_url()?>backend/index">Backend</a></li>
-            <?php } ?>
-            <?php } 
-            else { ?>
-            <li><a href="<?php echo base_url()?>home">Home</a></li>
-            <li><a href="<?php echo base_url()?>auth/login">Login</a></li>
-            <li><a href="<?php echo base_url()?>auth/register">Registration</a></li>
+      <ul class="nav navbar-nav">
+        <li><a class="navbar-hover" href="<?php echo base_url()?>appointments/index">Appointments</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle navbar-hover" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true"><?php echo $this->session->username;?> <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Account Details</a></li>
+            <li><a href="#">Wachtwoord vergeten</a></li>
+            <li><a href="<?php echo base_url()?>auth/logout">Logout</a></li>
+            <?php if ($this->session->userdata('role_id') != '3') {?>
+            <li role="separator" class="divider"></li>
+            <li><a href="<?php echo base_url()?>backend/index">Backend</a></li>
+            <?php }
+          }
+             else { ?>
+                   <ul class="nav navbar-nav">
+            <li><a class="navbar-hover" href="<?php echo base_url()?>auth/login">Login</a></li>
+            <li><a class="navbar-hover" href="<?php echo base_url()?>auth/register">Registration</a></li></ul>
            <?php }  
             ?>
           </ul>
-        </div>
-      </div>
-    </nav>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
