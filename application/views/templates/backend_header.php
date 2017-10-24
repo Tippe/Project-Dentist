971de51352a2cab39851dd89e3de0e318e40a6f6
Tippe van Roosmalen <tippevanroosmalen@outlook.com>
2017-10-24 13:06:07 +0200

    favicon in the backend

--- application/views/templates/backend_header.php
+++ application/views/templates/backend_header.php
@@ -12,6 +12,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="icon" href="<?php echo base_url(); ?>assets/img/brackets.png" type="image/gif" sizes="16x16">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="stylesheet" href="<?php echo base_url () . 'css/bootstrap.min.css';?>">
   <!-- Font Awesome -->
