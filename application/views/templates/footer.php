6233c851c5c67de89e23d75964b26dba7eb6c6e8
Armania50 <arya.00767@gmail.com>
2017-10-25 11:32:35 +0200

    home page is now good
    home page is now good

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,5 +1,5 @@
 <footer>
-	        <div class="footer">
+	        <div class="footer" style="text-align: center;">
 	          <a href=https://github.com/Tippe/Project-Dentist/graphs/contributors>Website created by the Code Addicts</a>
 	          <?php
 	          echo "<pre>";
