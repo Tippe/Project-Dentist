6d411e1c58ed6852e0ee22e04feb4d1192dd29ee
Lars Luijendijk <larsluijendijk@gmail.com>
2017-10-25 11:35:01 +0200

    merge fix

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,9 +1,6 @@
 <footer>
-<<<<<<< HEAD
 </div>
-=======
 	        <div class="footer" style="text-align: center;">
->>>>>>> f00bf148901cbea9abbc2b3d00f4dbebcdd08401
 	          <a href=https://github.com/Tippe/Project-Dentist/graphs/contributors>Website created by the Code Addicts</a>
 	          <?php
 	          echo "<pre>";
