0daf0c55fc84956f6faaf9153b28dc00c9abd186
Lars Luijendijk <larsluijendijk@gmail.com>
2017-10-30 12:49:26 +0100

    footer delete

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -3,9 +3,9 @@
 	<div class="footer" style="text-align: center;">
 	    <a href=https://github.com/Tippe/Project-Dentist/graphs/contributors>Website created by the Code Addicts</a>
 	    <?php
-    	echo '<pre>';
+/*    	echo '<pre>';
         var_dump($this->session);
-        echo '</pre>';
+        echo '</pre>';*/
 	    ?>
 	</div>
 </footer>
